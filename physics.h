611225955c8146f95eda2817f1e2c0c641b0442e
vboxuser <Rajapaksha R.A.C.J>
2026-09-03 12:06:03 +0000

    add physics function declaration to physics.h and fix missing semicolon error

--- physics.h
+++ physics.h
@@ -1,5 +1,5 @@
-double minimum_velocity();
-double maximum_velocity();
-double deg_rad(double angle);
-double max_angle();
-double range();
+double degToRad(double deg);
+double getDistance(Position p1, Position p2);
+double range(double speed, double teta);
+bool canHitTarget(Position shooter, Position target, double vMin, double vMax, double aMinDeg, double aMaxDeg, double *timeOfFlight);
+double getDegradedImpact(double baseImpact, double gamma, int shotsFired);
